dc812efc2920ac28fbca73b0812e1365f1453520
PalmTreeBlvd <55242169+PalmTreeBlvd@users.noreply.github.com>
2020-08-30 05:29:12 -0400

    Changing properties with useState. Commiting @ 39:53 for video

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from "react";
+// allows us to use state
+import React, { useState } from "react";
 import Tweet from "./Tweet";
 
 function App() {
+  const [isRed, setRed] = useState(false);
+  const [count, setCount] = useState(0);
+  // ** How you would use an object holding multiple properties **
+  // const [user, setUser] = useState({
+  //   name: "Ed",
+  //   age: 25,
+  //   posts: ["my first post", "my lovely summer"],
+  // });
+
+  const increment = () => {
+    setCount(count + 1);
+    setRed(!isRed);
+  };
+
   return (
     <div className="app">
-      <Tweet name="Dev Ed" message="hello everyone" />
-      <Tweet name="Petah" message="hello everyone 2" />
-      <Tweet name="Whateva" message="hello everyone 3" />
-      <Tweet name="Kyle" message="hello everyone 4" />
+      <h1 className={isRed ? "red" : ""}>Change my color!</h1>
+      <button onClick={increment}>Increment</button>
+      <h1>{count}</h1>
     </div>
   );
 }
